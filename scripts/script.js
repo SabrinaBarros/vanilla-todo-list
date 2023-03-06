476d75b3b44f2e90f38a2903265145954194f11f
SabrinaBarros <sabrina.barros2045@gmail.com>
2023-03-06 18:54:25 -0300

    feat(script): empty input validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,25 @@
 (() => {
 
+  const oopsText = document.getElementById('oopsText');
   const btn_add = document.getElementById('btn_add');
   const input = document.getElementById('input');
   const ul = document.getElementById('ul');
+  let totalTasks = document.getElementById('totalTasks');
+  let doneTasks = document.getElementById('doneTasks');
 
   let count = 0;
-
-  let doneTasks = document.getElementById('doneTasks');
-  let totalTasks = document.getElementById('totalTasks');
 
   btn_add.addEventListener('click', () => {
 
     count++;
-
     totalTasks.innerText++;
+
+    if (!input.value.length) {
+      if (oopsText.hasAttribute('hidden')) return oopsText.removeAttribute('hidden');
+      return;
+    }
+
+    if (!oopsText.hasAttribute('hidden') && input.value.length) oopsText.setAttribute('hidden', 'true');
 
     const li = document.createElement('li');
     li.setAttribute('id', 'row_' + count);
@@ -54,11 +60,7 @@
 
     });
 
-    checkbox.addEventListener('change', () => {
-
-      checkbox.checked ? doneTasks.innerText++ : doneTasks.innerText--
-
-    });
+    checkbox.addEventListener('change', () => checkbox.checked ? doneTasks.innerText++ : doneTasks.innerText--);
 
   });
 
